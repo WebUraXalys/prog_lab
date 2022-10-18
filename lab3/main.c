493322ca8d78effafa8e39689f2a89717ffb9fd6
xalys_ura <xalysjyra1992@gmail.com>
2022-10-18 12:48:42 +0300

    Виправлення синтаксису

--- lab3/main.c
+++ lab3/main.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
+int main()
+{
    float x, xMin, xMax, y;
    xMin = 0;
    xMax = 10;
    x = xMin;
-   while (x<=xMax)
+   while (x <= xMax)
    {
-      y=3*sin(x)+0.35*x-3.8;
+      y = 3 * sin(sqrt(x)) + 0.35 * x - 3.8;
       printf("Result: y = %f", y);
       printf(" x = %f\n", x);
       //++x;
       x = x + 0.5;
    }
-   
 }
