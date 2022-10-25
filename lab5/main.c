ff064dabc1896f24339c6d2e01415c879e55efaf
xalys_ura <xalysjyra1992@gmail.com>
2022-10-25 13:05:47 +0300

    Lab 5

---
+++ lab5/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+float fun(float x)
+{
+   return 3 * sin(sqrt(x)) + 0.35 * x - 3.8;
+}
+float fun2(float x)
+{
+   return (3 * cos(sqrt(x)) / 2 * sqrt(x)) + 0.35;
+}
+
+int main()
+{
+   float xMin, xMax, esp, c;
+   xMin = 2;
+   xMax = 3;
+   esp = 0.00001;
+   do
+   {
+      c = fun(xMin) / fun2(xMin);
+      xMin = xMin - c;
+   }
+   while (c > esp);
+   printf("x = %f fun(x) = %f \n", xMin, fun(xMin));
+}
