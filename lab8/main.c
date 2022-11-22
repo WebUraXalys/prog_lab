796f10cb603aa60e989616b382b69ab0e0102719
xalys_ura <xalysjyra1992@gmail.com>
2022-11-22 12:37:42 +0200

    new

--- lab8/main.c
+++ lab8/main.c
@@ -2,14 +2,16 @@
 #include "math.h"
 
 int main() {
-   float s, N;
+   float s, N, T;
    s = 0;
    N = 200;
+   T = 0.02;
    FILE *text = fopen("graph_lab8.txt", "w");
-   for (float t = 0; t < 0.02; t = 0.02/N)
+   for (float t = 0; t < 0.02; t = t+ (T/N))
    {
-      if (t > 0 && t <= 0.8){
-         s = (7*t) / 0.8;
+      if (t > 0 && t <= 0.8 * T ){
+         printf("%f \n", t);
+         s = 7*0.8*t / 0.8*t;
       };
       if (t > 0.8 && t <= 1)
       {
