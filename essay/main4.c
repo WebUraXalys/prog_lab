83487e4aa71986efb69da9c9be3b35066a5a5dc6
xalys_ura <xalysjyra1992@gmail.com>
2023-01-04 14:49:39 +0200

    New

---
+++ essay/main4.c
@@ -0,0 +1,19 @@
+#include "math.h"
+#include "stdio.h"
+
+int main(){
+   int a, b;
+   float S, s, h, x, N;
+   a = 0;
+   b = 4;
+   N = 1000;
+   S = 0;
+   h = (b - a)/ N;
+   for(int i = 1; i <= N; i++){
+      x = a + i * h;
+      s += (3 * x + 5);
+   }
+   S = h * s;
+   printf("%f \n", S);
+
+}
