ba191c2f695d0bd0242b5facb7dedc2e29143f1c
xalys_ura <xalysjyra1992@gmail.com>
2023-01-06 11:42:41 +0200

    Bag

--- essay/lab10_min_max.c
+++ essay/lab10_min_max.c
@@ -2,11 +2,8 @@
 #include <stdlib.h>
 #include <math.h>
 
-<<<<<<< HEAD
+
 int main()
-=======
-/*int main()
->>>>>>> 120e6bf6be9833e0a9ad268140ed868d034d879c
 {
     double Array[5]={3, 6, 1, 8, 12};
     int N=5;
@@ -20,7 +17,7 @@ int main()
             min_el = Array[i];
             ind_min_el = i;
         };
-<<<<<<< HEAD
+
     };
     printf("Minimum value in array is %f and its index is %d\n", min_el, ind_min_el);
     return 0;
@@ -44,28 +41,3 @@ int main()
 //     printf("Maximum value in array is %f and its index is %d\n", max_el, ind_max_el);
 //     return 0;
 // }
-=======
-    }
-    printf("Minimum value in array is %f and its index is %d\n", min_el, ind_min_el);
-    return 0;
-}*/
-
-int main()
-{
-    int N=5;
-    double Array[5]={3, 6, 1, 8, 12};
-    int i;
-    double max_el=Array[0];
-    int ind_max_el=0;
-    for(i=1; i<N; i++)
-    {
-        if(max_el<Array[i])
-        {
-            max_el = Array[i];
-            ind_max_el = i;
-        };
-    }
-    printf("Maximum value in array is %f and its index is %d\n", max_el, ind_max_el);
-    return 0;
-}
->>>>>>> 120e6bf6be9833e0a9ad268140ed868d034d879c
