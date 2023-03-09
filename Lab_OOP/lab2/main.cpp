73b26970a3cd298394ef4b653579dffb3855cd6f
xalys_ura <xalysjyra1992@gmail.com>
2023-03-09 11:07:58 +0200

    Create lab 2, rename lab 2 > lab 3

--- Lab_OOP/lab2/main.cpp
+++ Lab_OOP/lab2/main.cpp
@@ -1,50 +1,38 @@
 #include <iostream>
 #include <cmath>
+#include <stdio.h>
 
 using namespace std;
 
-class data
-{
-   // private:
-   //    float esp;
+class data{
    public:
-      // float esp = 0.0000001;
-      float xMin, xMax, esp, c;
+      float xMin, xMax, c, esp;
       float fun(float x){
          return 3 * sin(sqrt(x)) + 0.35 * x - 3.8;
       }
 
-      float fun2(float x) {
-         return (3 * cos(sqrt(x)) / 2 * sqrt(x)) + 0.35;
-      }
-      void fors(){
-         do
-         {
-            c = fun(xMin) / fun2(xMin);
-            xMin = xMin - c;
-         } while (c > esp);
-         cout << "x = " << xMin << " fun(x) = " << fun(xMin) << endl;
+      void cycle(){
+         while((xMax - xMin) > esp){
+            c = (xMin + xMax) / 2;
+            if (fun(c) * fun(xMin) < 0){
+               xMax = c;
+            }
+            if(fun(c) * fun(xMax) < 0){
+               xMin = c;
+            }
+         }
+         printf("x = %f fun(x) = %f \n", c, fun(c));
+         // cout << "x = " << c << " fun(x) = " << fun(c) << endl;
       }
 
 };
-
 int main(){
-   data obj1;
-   obj1.esp = 0.0000001;
-   obj1.xMin = 2;
-   obj1.xMax = 2.5;
-   obj1.fors();
-
-   data obj2;
-   obj2.esp = 0.0000001;
-   obj2.xMin = 7.5;
-   obj2.xMax = 8;
-   obj2.fors();
-   // do {
-   //    newObj.c = newObj.fun(newObj.xMin) /newObj.fun2(newObj.xMin);
-   //    newObj.xMin = newObj.xMin - newObj.c; 
-   // }
-   // while(newObj.c > newObj.esp);
-   // cout << "x = " << newObj.xMin << " fun(x) = " << newObj.fun(newObj.xMin) << endl;
+      data newObj;
+      newObj.esp = 0.00001;
+      newObj.xMin = 2;
+      newObj.xMax = 3;
+      newObj.cycle();
+      // newObj.x;
+      // newObj.fun();
 
 }
