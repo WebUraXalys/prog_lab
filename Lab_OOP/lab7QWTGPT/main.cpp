5f208b11d615c3848eb33c20b91bd4409b2f30b1
xalys_ura <xalysjyra1992@gmail.com>
2023-05-04 14:05:44 +0300

    lab Algo

--- Lab_OOP/lab7QWTGPT/main.cpp
+++ Lab_OOP/lab7QWTGPT/main.cpp
@@ -1,95 +1,63 @@
-// #include <QApplication>
-// #include <QWidget>
-// #include <qwt_plot.h>
-// #include <qwt_plot_curve.h>
-// #include <qwt_legend.h>
-// #include <qwt_plot_grid.h>
-// #include <qwt_symbol.h>
-// #include <cmath>
+#include <iostream>
+#include <QApplication>
+#include <QFile>
+#include <QTextStream>
+#include <cmath>
+#include <qwt_plot.h>
+#include <qwt_plot_curve.h>
+#include <QPen>
 
-// using namespace std;
+class Myclass
+{
+public:
+    double y, xMin, xMax;
+    void func(QwtPlot *plot)
+    {
+        QwtPlotCurve *curve = new QwtPlotCurve();
+        curve->setPen(QPen(Qt::blue));
 
-// class Graph : public QWidget
-// {
-// public:
-//    Graph(QWidget *parent = nullptr) : QWidget(parent)
-//    {
-//       setGeometry(100, 100, 500, 400);
+        QVector<double> xValues, yValues;
 
-//       // Create plot object
-//       plot = new QwtPlot(this);
-//       plot->setTitle("Plot of x and y values");
-//       plot->setCanvasBackground(Qt::white);
+        for (double x = xMin; x <= xMax; x += 0.1)
+        {
+            if (x >= -3 && x < -1.5)
+            {
+                y = 2;
+            }
+            else if (x >= -1.5 && x <= 1.5)
+            {
+               y = 4 - (x*x);
+            }
+            else if (x > 1.5 && x <= 3)
+            {
+                y = 2;
+            }
 
-//       // Create grid
-//       QwtPlotGrid *grid = new QwtPlotGrid();
-//       grid->attach(plot);
+            xValues.append(x);
+            yValues.append(y);
+        }
 
-//       // Create curve
-//       curve = new QwtPlotCurve();
-//       curve->setTitle("Data");
-//       curve->setPen(QPen(Qt::red));
-//       curve->setRenderHint(QwtPlotItem::RenderAntialiased, true);
+        curve->setSamples(xValues, yValues);
+        curve->attach(plot);
+    }
+};
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QwtPlot *plot = new QwtPlot();
+    plot->setTitle("My Plot");
+    plot->setCanvasBackground(Qt::white);
+    plot->setAxisTitle(QwtPlot::xBottom, "X Axis");
+    plot->setAxisTitle(QwtPlot::yLeft, "Y Axis");
+    plot->setAxisScale(QwtPlot::xBottom, -4, 4);
 
-//       // Create symbol
-//       QwtSymbol *symbol = new QwtSymbol(QwtSymbol::Ellipse,
-//                                         QBrush(Qt::white),
-//                                         QPen(Qt::black),
-//                                         QSize(8, 8));
-//       curve->setSymbol(symbol);
+    Myclass obj;
+    obj.xMin = -4;
+    obj.xMax = 4;
+    obj.func(plot);
 
-//       // Add data to curve
-//       curve->setSamples(x, y, 61);
+    plot->resize(600, 400);
+    plot->show();
 
-//       // Add legend
-//       QwtLegend *legend = new QwtLegend();
-//       plot->insertLegend(legend, QwtPlot::BottomLegend);
-
-//       // Display plot
-//       plot->replot();
-//    }
-
-// private:
-//    QwtPlot *plot;
-//    QwtPlotCurve *curve;
-//    float x[61], y[61];
-
-// public:
-//    void plot_data()
-//    {
-//       int count = 0;
-//       for (float i = -3; i <= 3; i += 0.1)
-//       {
-//          if(i >= -3 && i < -1.5){
-//             y[count] = 2;
-//          } else if (i >= -1.5 && i <= 1.5)
-//          {
-//             y[count] = 4 - (i*i);
-//          } else if (i > 1.5 && i <= 3)
-//          {
-//             y[count] = 2;
-//          }
-//          x[count] = i;
-//          count++;
-//       };
-
-//       curve->setSamples(x, y, 61);
-//       plot->replot();
-//    }
-// };
-
-// int main(int argc, char *argv[])
-// {
-//    QApplication a(argc, argv);
-
-//    // Create graph object
-//    Graph graph;
-
-//    // Plot data
-//    graph.plot_data();
-
-//    // Show graph
-//    graph.show();
-
-//    return a.exec();
-// }
+    return app.exec();
+}
